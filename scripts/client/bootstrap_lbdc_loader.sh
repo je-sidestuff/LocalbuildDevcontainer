844cebf35eb343792ab0208a51ae418b40a84ed9
forjor <jordan.interviews@gmail.com>
2024-06-21 21:09:00 -0300

    feat: Updates to make gitfile source type function correctly.

--- scripts/client/bootstrap_lbdc_loader.sh
+++ scripts/client/bootstrap_lbdc_loader.sh
@@ -13,8 +13,43 @@ source ${SCRIPT_DIR}/../lbdc_repo_config.sh
 case $LBDC_LOADER_RETRIEVAL_STRATEGY in
 
   gitfile)
-    echo "Installing from git source is not yet supported."
-    exit 1
+    # Check for loader cache hit
+    GIT_HASH=$(curl -L   -H "Accept: application/vnd.github+json" \
+                         -H "X-GitHub-Api-Version: 2022-11-28" \
+                         https://api.github.com/repos/je-sidestuff/LocalbuildDevcontainer/commits?sha=feat/first_draft \
+                         | tac | tac | head -n 3 | tail -n 1 | cut -d '"' -f 4)
+
+    if [ -f ${SCRIPT_DIR}/.lbdc-cache/git-hash ]; then
+
+      echo "Cached version for $GIT_HASH found."
+      if [[ $(< ${SCRIPT_DIR}/.lbdc-cache/git-hash) == $GIT_HASH ]]; then
+
+        echo "Git hash for retrieval matches cache."
+
+        # Verify files as well as git hash
+        CHECKSUM=`find ${SCRIPT_DIR}/.lbdc-cache ! -name '*files-csum' -type f -exec md5sum "{}" +`
+
+        if [[ $(< ${SCRIPT_DIR}/.lbdc-cache/files-csum) == $CHECKSUM ]]; then 
+            echo "LBDC loader fileset matches cache."
+            exit 0
+        fi
+      fi
+    fi
+
+    echo "Retrieving LBDC loader with strategy $LBDC_LOADER_RETRIEVAL_STRATEGY."
+    rm -rf ${SCRIPT_DIR}/.lbdc-cache
+    mkdir -p ${SCRIPT_DIR}/.lbdc-cache
+    curl -L https://github.com/je-sidestuff/LocalbuildDevcontainer/archive/refs/heads/feat/first_draft.zip -o ${SCRIPT_DIR}/.lbdc-cache/lbdc.zip
+    unzip ${SCRIPT_DIR}/.lbdc-cache/lbdc.zip -d ${SCRIPT_DIR}/.lbdc-cache/
+    rm ${SCRIPT_DIR}/.lbdc-cache/lbdc.zip
+    mv ${SCRIPT_DIR}/.lbdc-cache/LocalbuildDevcontainer-feat-first_draft/* ${SCRIPT_DIR}/.lbdc-cache/
+    mv ${SCRIPT_DIR}/.lbdc-cache/LocalbuildDevcontainer-feat-first_draft/.* ${SCRIPT_DIR}/.lbdc-cache/
+    rm -rf ${SCRIPT_DIR}/.lbdc-cache/LocalbuildDevcontainer-feat-first_draft
+    echo -n "$GIT_HASH" > ${SCRIPT_DIR}/.lbdc-cache/git-hash
+    
+    # Regenerate checksum to include git hash now that is has been written
+    CHECKSUM=`find ${SCRIPT_DIR}/.lbdc-cache ! -name '*files-csum' -type f -exec md5sum "{}" +`
+    echo -n "$CHECKSUM" > ${SCRIPT_DIR}/.lbdc-cache/files-csum
     ;;
 
   submodule)
@@ -23,7 +58,26 @@ case $LBDC_LOADER_RETRIEVAL_STRATEGY in
     ;;
 
   local)
-    # TODO - call checksum check from here?
+    # Check for loader cache hit
+    # TODO - this method works only for filesystem based retrieval strategies
+    CHECKSUM=`find $LBDC_LOADER_SOURCE_LOCATION ! -name '*files-csum' -type f -exec md5sum "{}" +`
+
+    # Bring loader into cache if necessary
+    if [ -f ${SCRIPT_DIR}/.lbdc-cache/file-csum ]; then
+
+        echo "LBDC loader fileset cache present."
+
+        if [[ $(< ${SCRIPT_DIR}/.lbdc-cache/file-csum) == $CHECKSUM ]]; then 
+            echo "LBDC loader fileset matches cache."
+            exit 0
+        fi
+    fi
+
+    echo "Retrieving LBDC loader with strategy $LBDC_LOADER_RETRIEVAL_STRATEGY."
+    rm -rf ${SCRIPT_DIR}/.lbdc-cache
+    mkdir -p ${SCRIPT_DIR}/.lbdc-cache
+    cp -r $LBDC_LOADER_SOURCE_LOCATION/* ${SCRIPT_DIR}/.lbdc-cache
+    echo -n "$CHECKSUM" > ${SCRIPT_DIR}/.lbdc-cache/file-csum
     ;;
 
   *)
@@ -32,19 +86,4 @@ case $LBDC_LOADER_RETRIEVAL_STRATEGY in
     ;;
 esac
 
-# Check for loader cache hit
-# TODO - this method works only for filesystem based retrieval strategies
-CHECKSUM=`find $LBDC_LOADER_SOURCE_LOCATION -type f -exec md5sum "{}" +`
 
-# Bring loader into cache if necessary
-if [ -f ${SCRIPT_DIR}/.lbdc-cache-csum ]; then
-    if [[ $(< ${SCRIPT_DIR}/.lbdc-cache-csum) == $CHECKSUM ]]; then 
-        echo "LBDC loader is already cached."
-        exit 0
-    fi
-fi
-
-echo "Retrieving LBDC loader."
-mkdir -p ${SCRIPT_DIR}/.lbdc-cache
-cp -r $LBDC_LOADER_SOURCE_LOCATION/* ${SCRIPT_DIR}/.lbdc-cache
-echo -n "$CHECKSUM" > ${SCRIPT_DIR}/.lbdc-cache-csum
--- scripts/templates/config/lbdc_repo_config.sh
+++ scripts/templates/config/lbdc_repo_config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export LBDC_LOADER_RETRIEVAL_STRATEGY="local"
+export LBDC_LOADER_RETRIEVAL_STRATEGY="gitfile"
 export LBDC_LOADER_SOURCE_LOCATION="/home/jedsall/workspace/sandbox/LocalbuildDevcontainer"
