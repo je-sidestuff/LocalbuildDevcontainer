5c08a46b7175afa2f51c29dd2337b37203d70146
forjor <jordan.interviews@gmail.com>
2024-12-01 13:09:08 -0400

    feat: Continue working on the git install/test/load paths.

--- scripts/client/install_lbdc.sh
+++ scripts/client/install_lbdc.sh
@@ -80,8 +80,8 @@ case $LBDC_INSTALL_SOURCE_TYPE in
     curl -L https://github.com/je-sidestuff/LocalbuildDevcontainer/archive/refs/heads/${LBDC_INSTALL_GIT_LOCATION}.zip -o ${LBDC_WORKING_LOCATION}/.lbdc-install/lbdc.zip
     echo "Calling: unzip ${LBDC_WORKING_LOCATION}/.lbdc-install/lbdc.zip -d ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer"
     unzip ${LBDC_WORKING_LOCATION}/.lbdc-install/lbdc.zip -d ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer
-    mv ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/LocalbuildDevcontainer-feat-first_draft/* ${SCRIPT_DIR}/.lbdc-install/
-    mv ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/LocalbuildDevcontainer-feat-first_draft/.* ${SCRIPT_DIR}/.lbdc-install/
+    mv ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/LocalbuildDevcontainer-feat-first_draft/* ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/
+    mv ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/LocalbuildDevcontainer-feat-first_draft/.* ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/
     rm -rf ${LBDC_WORKING_LOCATION}/LocalbuildDevcontainer/LocalbuildDevcontainer-feat-first_draft
     exit 1
     ;;
